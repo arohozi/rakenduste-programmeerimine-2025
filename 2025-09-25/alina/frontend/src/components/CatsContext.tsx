b1bb97f8df408b5083b37fd3ef9a8fa3feaf3583
Alina Rohozinska <arohozi@tlu.ee>
2025-10-07 15:40:41 +0300

    Update CatsContext.tsx

--- 2025-09-25/alina/frontend/src/components/CatsContext.tsx
+++ 2025-09-25/alina/frontend/src/components/CatsContext.tsx
@@ -1,6 +1,5 @@
 import { Box, List, ListItem, Typography } from "@mui/material";
 import React, { useEffect, useState, createContext, useContext } from "react";
-
 type Cat = {
   id: string;
   name: string;
